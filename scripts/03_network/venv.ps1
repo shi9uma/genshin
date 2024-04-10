c4533b7b3b7577cf5437b2c3eabaadcd1bd15ff9
wkyuu <tataramoriko@163.com>
2024-04-10 16:42:41 +0800

    network tools

---
+++ scripts/03_network/venv.ps1
@@ -0,0 +1,4 @@
+
+& venv_network/Scripts/Activate.ps1
+write-host "venv_network activated" -ForegroundColor Yellow
+write-host "type 'deactivate' to deactivate the virtual environment" -ForegroundColor Yellow

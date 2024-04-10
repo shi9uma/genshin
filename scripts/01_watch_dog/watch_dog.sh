b2d143663d631fae593227a97c0230b3cdd1e659
wkyuu <wkyuu@majo.im>
2024-04-10 18:16:48 +0800

    update watchdog

--- scripts/01_watch_dog/watch_dog.sh
+++ scripts/01_watch_dog/watch_dog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 监测 vps 突然高占用的情况, 将其记录到日志文件中供排查
+# 监测 vps 突然高占用的情况
 LOGFILE="/var/log/xxx_watchdog.log"
-CPU_USAGE_THRESHOLD=75  # top, cpu 占用阈值
-MEMORY_USAGE_THRESHOLD=75   # free, 内存占用阈值
+CPU_USAGE_THRESHOLD=75  # CPU 占用阈值
+MEMORY_USAGE_THRESHOLD=75   # 内存占用阈值
 
 while true
 do
@@ -11,7 +11,12 @@ do
     MEMORY_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
     if (( $(echo "$CPU_USAGE > $CPU_USAGE_THRESHOLD" | bc -l) )) || (( $(echo "$MEMORY_USAGE > $MEMORY_USAGE_THRESHOLD" | bc -l) ))
     then
-        echo "$(date) - high resource usage detected: CPU: $CPU_USAGE%, Memory: $MEMORY_USAGE%" >> $LOGFILE
+        echo "$(date) - high resource usage detected: CPU: $CPU_USAGE%, Memory: $MEMORY_USAGE%" >> $LOGFILE # 高占用信息
+        echo "Top CPU consuming processes:" >> $LOGFILE
+        ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu | head -n 5 >> $LOGFILE  # CPU 占用最高的进程
+        
+        echo "Top memory consuming processes:" >> $LOGFILE
+        ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head -n 5 >> $LOGFILE  # 内存占用最高的进程信息
     fi
     sleep 60    # 60s
 done

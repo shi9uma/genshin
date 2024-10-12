bcff9c304b1a2b2be3a680b808cfd5ddbb177956
wkyuu <wkyuu@majo.im>
2024-10-12 18:45:35 +0800

    update1xia

--- script/01-misc/01-template.py
+++ script/01-misc/01-template.py
@@ -11,7 +11,32 @@ args = vars(ap.parse_args())
 args['dir'] # 取值
 
 
-# ============================== color ============================== #
+# ============================== misc ============================== #
+def exec(cmd: str, print_output=False):
+    '''
+    直接传入命令行字符串，执行命令行并返回输出
+    ```python
+    exec('ls -l')
+    exec('ls -l', print_output=True)
+    ```
+    '''
+    assert type(cmd) == str, 'wrong cmd'
+    import subprocess
+    output = subprocess.check_output(cmd, shell=True)
+    if print_output: print(output)
+
+def debug(*args):
+    '''
+    打印传入的参数值，并显示其在源码的文件和行号
+    '''
+    import inspect
+    frame = inspect.currentframe().f_back
+    info = inspect.getframeinfo(frame)
+    print(f"{color(clean_path(info.filename), 3)}: {color(info.lineno, 4)} {color('|', 7)}", end = ' ')
+    for x in args:
+        print(f"{color(x, 2)}", end = ' ' if x[-1] != ' ' else '')
+    print('')
+
 def color(text: str = '', color: int = 2) -> str:
     '''
     返回对应的控制台 ANSI 颜色; 
@@ -41,6 +66,46 @@ def color(text: str = '', color: int = 2) -> str:
         8: '\033[1;37m{}\033[0m',   # 白色加粗
     }
     return color_table[color].format(text)
+
+# ============================== path ============================== #
+def clean_path(path: str) -> str:
+    '''
+    清理反斜杠
+    '''
+    return path.replace('\\', '/').lower()
+
+def get_dirname(path: str) -> str:
+    '''
+    返回路径的目录名
+    '''
+    import os
+    return clean_path(os.path.dirname(path))
+
+def get_fullpath(path: str) -> str:
+    '''
+    返回路径的绝对路径
+    '''
+    import os
+    return clean_path(os.path.abspath(path))
+
+def get_workdir() -> str:
+    '''
+    返回根工作目录
+    '''
+    return get_fullpath(get_dirname(get_dirname(__file__)))
+
+def xpath(root_path: str, *args: str) -> str:
+    '''
+    将所有传入的参数按顺序组合成路径
+    '''
+    import os
+    for path in args:
+        if type(path) != str:
+            if type(path) == int: path = str(path)
+            else: continue
+        path = path.replace('\\', '').replace('/', '')
+        root_path = os.path.join(root_path, path)
+    return clean_path(root_path)
 
 # ============================== split line ============================== #
 SWITCH = {

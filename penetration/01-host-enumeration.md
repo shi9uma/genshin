0b78843bb81549c788654a6f0e7385785e196fac
wkyuu <wkyuu@majo.im>
2024-11-19 18:07:05 +0800

    update1xia

---
+++ penetration/01-host-enumeration.md
@@ -0,0 +1,13 @@
+# host-enumeration
+
+主机信息搜集
+
+
+
+
+
+
+
+## refer
+
+1. https://blog.g0tmi1k.com/2011/08/basic-linux-privilege-escalation/
--- script/03-network/04-shodan.py
+++ script/03-network/04-shodan.py
@@ -2,6 +2,7 @@
 
 import shodan
 import os
+import json
 
 api_storage_name = '.shodan'
 if os.environ.get('USERPROFILE') != None:
@@ -11,5 +12,33 @@ elif os.environ.get('HOME') != None:
 else:
     api_storage_path = f'{os.path.dirname(os.path.abspath(__file__))}/{api_storage_name}'
 
-def check_api():
-    pass
+class shodan_client:
+    def __init__(self):
+        self.api_storage_path = api_storage_path
+        self.api_key = self.check_api_key()
+    
+    def init_api_key(self):
+        
+        pass
+
+    def check_api_key(self) -> str:
+        
+        api_json = json.loads(open(self.api_storage_path, 'r').read())
+        return api_json['api_key']
+
+def color(text: str = '', color: int = 2) -> str:
+    '''
+    返回对应的控制台 ANSI 颜色
+    '''
+    color_table = {
+        0: '{}',                    # 无色
+        1: '\033[1;30m{}\033[0m',   # 黑色加粗
+        2: '\033[1;31m{}\033[0m',   # 红色加粗
+        3: '\033[1;32m{}\033[0m',   # 绿色加粗
+        4: '\033[1;33m{}\033[0m',   # 黄色加粗
+        5: '\033[1;34m{}\033[0m',   # 蓝色加粗
+        6: '\033[1;35m{}\033[0m',   # 紫色加粗
+        7: '\033[1;36m{}\033[0m',   # 青色加粗
+        8: '\033[1;37m{}\033[0m',   # 白色加粗
+    }
+    return color_table[color].format(text)
